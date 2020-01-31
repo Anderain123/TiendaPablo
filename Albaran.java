0f715443c699262a81c8abb044666fe1381eda75
TeParadoX <david_lopez_uru@hotmail.com>
2020-01-31 18:34:46 +0100

    Penes gordos

--- Albaran.java
+++ Albaran.java
@@ -2,14 +2,14 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
 /**
- * Almacena pedidos en formato HashMap
+ * Almacena pedidos en formato HashMap en un albarán
  *
  * @author David L.F.
  * @version (a version number or a date)
  */
 public class Albaran
 {
-    
+
     HashMap<Long, Pedido> albaran;
     /**
      * Constructor for objects of class Albaran
@@ -17,26 +17,38 @@ public class Albaran
     public Albaran()
     {
         albaran = new HashMap<>();
-        
+
     }
 
     /**
-     * An example of a method - replace this comment with your own
+     * Agrega un pedido al albaran
      *
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
     public void meterPedido(Pedido pedidito)
     {
-      albaran.put(pedidito.getId(),pedidito);  
+        albaran.put(pedidito.getId(),pedidito);  
     }
+
+    /**
+     * Lista todos los pedidos almacenados en el albaran
+     * @param nada
+     * @return lista imprime albaran
+     */
     public void listarPedidos(){
-    Iterator<Long> it= albaran.keySet().iterator();
-    while(it.hasNext()){
-        System.out.println(albaran.get(it.next()).toString());
+        Iterator<Long> it= albaran.keySet().iterator();
+        while(it.hasNext()){
+            System.out.println(albaran.get(it.next()).toString());
+        }
+
     }
-    
-    }
+    /**
+     * Muestra un pedido especifico del albaran
+     * 
+     * @param long 
+     * @return devuelte el valor de la clave calvedelPedido del albaran
+     */
     public void pedidoEspecifico(long clavedelPedido){
         System.out.println(albaran.get(clavedelPedido).toString());
     }
